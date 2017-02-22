07922e76d0421afddbc93e94e81526642c934eb1
Marcelo Camargo <marcelocamargo@linuxmail.org>
2017-02-22 14:33:48 -0300

    Use colors on vm

--- src/vm.ts
+++ src/vm.ts
@@ -1,12 +1,20 @@
-/// <reference path="../typings/globals/node/index.d.ts"; />
+/// <reference path="../typings/index.d.ts"; />
 import * as readline from 'readline';
 import * as Bluebird from 'bluebird';
+import 'colors';
 
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-const question = Bluebird.promisify((text, callback) => {
+const getLine = Bluebird.promisify((text, callback) => {
     rl.question(text, callback.bind(null, null));
 });
+
+const putStrLn = Bluebird.promisify((text, callback) => {
+    rl.write(text + '\n');
+    callback();
+});
+
+putStrLn('Setting up virtual machine...'.blue);
--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,1 +1,2 @@
 /// <reference path="globals/node/index.d.ts" />
+/// <reference path="custom/colors.d.ts" />
